dbebf444952a1c4cc920f6aef46aef33e89cef28
Cons Wang <conswang@seas.upenn.edu>
2022-05-09 03:16:17 -0400

    working chart

---
+++ frontend/src/style/colors.ts
@@ -0,0 +1,8 @@
+export const BLUE_BRIGHT = '#1874D2';
+export const BLUE_MED = '#0141CF';
+export const BLUE_DARK = '#040084';
+export const BLUE_LIGHT = '#669EFF';
+
+export const ORANGE_DARK = '#FD6600';
+export const ORANGE_MED = '#FD7F00';
+export const ORANGE_LIGHT = '#FECC98';
