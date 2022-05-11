7ef9e8152a2d7fca89efbf4790b89b1b8a775778
Henry Chen <henchen@seas.upenn.edu>
2022-05-10 21:21:54 -0400

    changed deployment HOST

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,3 @@
 // for Accessing Backend
-export const HOST = 'http://localhost:8000'; // https://pacific-cove-61696.herokuapp.com // TODO change back
+export const HOST = 'http://ec2-44-204-65-116.compute-1.amazonaws.com:45555';
+// export const HOST = 'http://localhost:8000'; // https://pacific-cove-61696.herokuapp.com // TODO change back
